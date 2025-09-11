1f288a76738828f2c16b87b318bdb61f5416d68c
rizal111 <shahrizaldev01@gmail.com>
2025-09-11 22:04:28 +0800

    feat: change app logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,10 +1,10 @@
-import { Binoculars } from 'lucide-react';
+import { Factory } from 'lucide-react';
 
 export default function AppLogo() {
     return (
         <>
             <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <Binoculars className="text-black" />
+                <Factory className="text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">Manufacturing Monitoring</span>
